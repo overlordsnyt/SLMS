39c904e65174b6486e6d6277aff12d1ea2923e19
overlord <506421403@qq.com>
2016-05-14 10:45:04 +0800

    关系实体及其hibernate数据库映射文件

---
+++ src/com/slms/domain/ConnectionTest.java
@@ -0,0 +1,34 @@
+package com.slms.domain;
+/**
+ * 测试系统基本连通性的测试实体类
+ * @author overlord
+ *
+ */
+public class ConnectionTest {
+	private String username,password;
+	private int id;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
